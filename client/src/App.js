a41bd7e7462c703340f68386bb5398cfc0b59f92
Shakib448 <shakiba448@gmail.com>
2023-03-12 21:37:16 +0600

    WIP ON error

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,40 @@
-import './App.css';
+import "./App.css";
 
-import 'flowbite';
-import Home from 'src/components/Home';
-import Login from './components/Login/Login';
-import CreateAccounts from './components/CreateAccount/CreateAccounts';
-import { Admin } from './components/Admin/Admin';
-import ManageProduct from './components/dashBoardComponents/ManageProduct/ManageProduct';
-import CheckOut from './components/checkout/CheckOut';
-import AddProduct from './components/dashBoardComponents/addProduct/AddProduct';
-import NavBar from './components/NavBar/NavBar';
-import SearchBar from './components/SearchBar/SearchBar';
-import Shop from './components/Shop/Shop';
-import Main from './Layout/Main'
-import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
+import "flowbite";
+import Home from "src/components/Home";
+import Login from "./components/Login/Login";
+import CreateAccounts from "./components/CreateAccount/CreateAccounts";
+import { Admin } from "./components/Admin/Admin";
+import ManageProduct from "./components/dashBoardComponents/ManageProduct/ManageProduct";
+import CheckOut from "./components/checkout/CheckOut";
+import AddProduct from "./components/dashBoardComponents/addProduct/AddProduct";
+import NavBar from "./components/NavBar/NavBar";
+import SearchBar from "./components/SearchBar/SearchBar";
+import Shop from "./components/Shop/Shop";
+import Main from "./Layout/Main";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: '/', element: <Main></Main>,
+      path: "/",
+      element: <Main></Main>,
       children: [
-        { path: '/', element: <Shop></Shop> },
-        { path: '/order', element: <CheckOut></CheckOut> }
-      ]
+        { path: "/home", element: <Shop></Shop> },
+        { path: "/order", element: <CheckOut></CheckOut> },
+      ],
     },
-    { path: '/login', element: <Login></Login> },
+    { path: "/login", element: <Login></Login> },
     {
-      path: '/admin', element: <Admin></Admin>,
+      path: "/admin",
+      element: <Admin></Admin>,
       children: [
-        { path: '/admin', element: <ManageProduct></ManageProduct> },
-        { path: '/admin/addProduct', element: <AddProduct></AddProduct> }
-      ]
+        { path: "/admin", element: <ManageProduct></ManageProduct> },
+        { path: "/admin/addProduct", element: <AddProduct></AddProduct> },
+      ],
     },
-    { path: '/register', element: <CreateAccounts></CreateAccounts> }
-  ])
+    { path: "/register", element: <CreateAccounts></CreateAccounts> },
+  ]);
   return (
     <div className="App">
       {/* <NavBar />
@@ -46,7 +48,6 @@ function App() {
       {/* <CheckOut /> */}
       {/* <AddProduct /> */}
       <RouterProvider router={router} />
-
     </div>
   );
 }
